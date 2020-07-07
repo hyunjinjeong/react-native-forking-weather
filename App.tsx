d91d4a2e2af543c62248c64d11462b7162e32fb0
hyunjinjeong <hyunjin.dev@gmail.com>
2020-07-08 00:02:06 +0900

    Get location

--- App.tsx
+++ App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
+import { Alert } from 'react-native';
 
 import Loading from './src/Loading';
 
-export default function App() {
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getLocation = async () => {
+      try {
+        setIsLoading(true);
+        await Location.requestPermissionsAsync();
+        const { coords: { latitude, longitude }} = await Location.getCurrentPositionAsync();
+        console.log(latitude, longitude);
+        setIsLoading(false);
+      }
+      catch {
+        Alert.alert("Permission denied.");
+      }
+    }
+    getLocation();
+  }, []);
+
   return (
-    <Loading />
+    isLoading && <Loading />
   );
 };
+
+export default App;
